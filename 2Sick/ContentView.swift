55393dde44af18228b8bd84ce8111777db05953e
mhg77 <hashier@yandex.ru>
2026-07-20 19:56:41 +0300

    Initial Commit

---
+++ 2Sick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2Sick
+//
+//  Created by Михаил Асаилов on 20.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2Sick/_SickApp.swift
@@ -0,0 +1,17 @@
+//
+//  _SickApp.swift
+//  2Sick
+//
+//  Created by Михаил Асаилов on 20.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct _SickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
